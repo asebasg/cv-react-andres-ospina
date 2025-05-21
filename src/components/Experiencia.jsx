1fc883e04d9b24a7e58c6a90d2ebf53f51da3108
Sebastian Ospina <asebasg07@gmail.com>
2025-05-21 13:51:53 -0500

    Componente: Experiencia.jsx, experiencia con 4 cargos definidos

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,21 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        <li>
+          <strong>Desarrollador Full Stack</strong> - Navisoft Gaming (2022-2024)
+        </li>
+        <li>
+          <strong>Instructor TIC</strong> - SENA (2020-2022)
+        </li>
+        <li>
+            <strong>Creador del aplicativos web y aplicaciones móviles</strong> - Axomy Solutions & Co. (2022 - 2024)
+        </li>
+        <li>
+            <strong>Creador de sistemas de software de prevención de riesgos</strong> - Área Metropolitana del Valle de Aburrá (2024 - 2025)
+        </li>
+      </ul>
+    </section>
+  );
+}
