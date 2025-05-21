a1296df226c0438eb6880fb79d7ced5575cf36cf
Sebastian Ospina <asebasg07@gmail.com>
2025-05-21 14:00:50 -0500

    Componente: Educación.jsx, con educación y formación complementaria

---
+++ src/components/Educacion.jsx
@@ -0,0 +1,21 @@
+export default function Educacion() {
+  return (
+    <section>
+      <h3>Educación</h3>
+      <ul>
+        <li>
+          <strong>Ingeniería de Datos y Software</strong> - Universidad de San Buenaventura (2027-2030)
+        </li>
+        <li>
+            <strong>Tecnólogo en Análisis y Desarrollo de Software</strong> - Servicio Nacional de Aprendizaje (2025-2026)
+        </li>
+        <li>
+            <strong>Bachiller técnico en Sistemas con especialidad en Desarrollo de Software</strong> - Institución Educativa Atanasio Girardot (2022-2024)
+        </li>
+        <li>
+            <strong>Diplomado de Programación en PHP</strong> - Politécnico Grancolombiano (2024)
+        </li>
+      </ul>
+    </section>
+  );
+}
