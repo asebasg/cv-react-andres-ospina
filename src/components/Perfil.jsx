7d60e1cb5bd49c28dfc88374c6c0b8604ded30ff
Sebastian Ospina <asebasg07@gmail.com>
2025-05-21 13:46:15 -0500

    Componente: Perfil.jsx, con descripción personalizada.

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,11 @@
+export default function Perfil() {
+  return (
+    <section>
+      <h3>Perfil</h3>
+      <p>
+        Bachiller técnico en Sistemas, con énfasis en Desarrollo de Software, tecnólogo en Analisis y Desarrollo de software con experiencia en
+        desarrollo web y móvil, Ingeniero de Datos y Software con especialidad en Desarrollo de Inteligencia Artificial, apasionado por la tecnología.
+      </p>
+    </section>
+  );
+}
