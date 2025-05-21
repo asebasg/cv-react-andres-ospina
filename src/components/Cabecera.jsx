157815a6552ccd7691311d37d81944956e367dd5
Sebastian Ospina <asebasg07@gmail.com>
2025-05-21 13:36:25 -0500

    Componente: Cabecera.jsx con datos personales actualizados

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,12 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Andrés Sebastián Ospina Guzmán</h1>
+      <h2>Desarrollador FullStack Senior</h2>
+      <h4>Datos de contacto:</h4>
+      <p><strong>Teléfono</strong>: 3225316150</p>
+      <p><strong>Correo</strong>: asebasg07@gmail.com</p>
+      <p><strong>Ciudad</strong>: Medellín</p>
+    </header>
+  );
+}
