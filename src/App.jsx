c803838be601f37c18c9487f4e81c67159779c18
Sebastian Ospina <asebasg07@gmail.com>
2025-05-21 14:11:50 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Cabecera from "./components/Cabecera.jsx";
+import Perfil from "./components/Perfil.jsx";
+import Educacion from "./components/Educacion.jsx";
+import Experiencia from "./components/Experiencia.jsx";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <Cabecera />
+      <Perfil />
+      <Educacion />
+      <Experiencia />
+    </div>
   );
 }
 
-export default App;
+export default App
