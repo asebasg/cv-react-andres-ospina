45da1d4cbb1581f61dad00aad5c1f9a0434a4a72
Sebastian Ospina <asebasg07@gmail.com>
2025-05-26 14:32:06 -0500

    Componente: Educacion.css, añadiendo formato a los headers.
    Componente: Educacion.jsx, añadiendo dos const para la educacion y las instituciones.

--- src/components/Educacion.jsx
+++ src/components/Educacion.jsx
@@ -1,19 +1,35 @@
+import "./Educacion.css" 
+
+const educacion = {
+  superior: "Ingeniería de Datos y Software",
+  tecnologia: "Tecnólogo en Análisis y Desarrollo de Software",
+  bachillerato: "Bachiller técnico en Sistemas con especialidad en Desarrollo de Software",
+  otros: "Diplomado de Programación en PHP",
+};
+
+const instituciones = {
+  ucc: "Universidad Coperativa de Colombia",
+  sena: "Servicio Nacional de Aprendizaje",
+  ieag: "I.E. Atanasio Girardot",
+  pg: "Politecnico Grancolombiano"
+}
+
 export default function Educacion() {
   return (
     <section>
       <h3>Educación</h3>
       <ul>
         <li>
-          <strong>Ingeniería de Datos y Software</strong> - Universidad de San Buenaventura (2027-2030)
+          <strong>{educacion.superior}</strong> - {instituciones.ucc} (2027-2029)
         </li>
         <li>
-            <strong>Tecnólogo en Análisis y Desarrollo de Software</strong> - Servicio Nacional de Aprendizaje (2025-2026)
+            <strong>{educacion.tecnologia}</strong> - {instituciones.sena} (2025-2026)
         </li>
         <li>
-            <strong>Bachiller técnico en Sistemas con especialidad en Desarrollo de Software</strong> - Institución Educativa Atanasio Girardot (2022-2024)
+            <strong>{educacion.bachillerato}</strong> - {instituciones.ieag} (2022-2024)
         </li>
         <li>
-            <strong>Diplomado de Programación en PHP</strong> - Politécnico Grancolombiano (2024)
+            <strong>{educacion.otros}</strong> - {instituciones.pg} (2024)
         </li>
       </ul>
     </section>
