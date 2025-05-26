8aba9137e6bad48a5eb98b9f93d9c03219294a4a
Sebastian Ospina <asebasg07@gmail.com>
2025-05-26 14:19:06 -0500

    Componente: Cabecera.css, estilo añadido para la cabecera.

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,12 +1,28 @@
+import "./Cabecera.css" 
+
+const user = {
+  nombre: "Andrés Sebastián Ospina Guzmán",
+  cargo: "Desarrollador FullStack Senior",
+  telefono: "3225316150",
+  correo: "asebasg07@gmail.com",
+  ciudad: "Medellín",
+};
+
 export default function CabeceraCV() {
   return (
     <header>
-      <h1>Andrés Sebastián Ospina Guzmán</h1>
-      <h2>Desarrollador FullStack Senior</h2>
+      <h1>{user.nombre}</h1>
+      <h2>{user.cargo}</h2>
       <h4>Datos de contacto:</h4>
-      <p><strong>Teléfono</strong>: 3225316150</p>
-      <p><strong>Correo</strong>: asebasg07@gmail.com</p>
-      <p><strong>Ciudad</strong>: Medellín</p>
+      <p>
+        <strong>Teléfono</strong>: {user.telefono}
+      </p>
+      <p>
+        <strong>Correo</strong>: {user.correo}
+      </p>
+      <p>
+        <strong>Ciudad</strong>: {user.ciudad}
+      </p>
     </header>
   );
 }
