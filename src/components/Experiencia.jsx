ad3b9ca8185bdc6c6ced36840b272f3866f58d7f
Sebastian Ospina <asebasg07@gmail.com>
2025-05-28 14:22:39 -0500

    Componente: Experiencia.jsx, actualizado para añadir array de objetos con todos los datos de la experiencia laboral.

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,34 +1,89 @@
 import "./Experiencia.css";
 
-const experiencia = {
-  apps: "Creador del aplicativos web y aplicaciones móviles",
-  tecnologias: "Creador de sistemas de software de prevención de riesgos",
-  asociaciones:
-    "Colaborador en proyectos de software y produccion de contenido",
-};
-
-const empresas = {
-  axomy: "Axomy Solutions & Co.",
-  amva: "Área Metropolitana del Valle de Aburrá",
-  alcgir: "Alcaldía de Girardota",
-};
-
 export default function Experiencia() {
+  const experiencia = [
+    {
+      id: 1,
+      puesto: "Desarrollador Full Stack",
+      empresa: "ToGrow Agencia",
+      periodo: "2026 - Actualidad",
+    },
+    {
+      id: 2,
+      puesto: "Lider de proyectos de desarrollo de software",
+      empresa: "Gobernacion de Antioquia",
+      periodo: "2024 - 2026",
+    },
+    {
+      id: 3,
+      puesto:
+        "Creador de aplicativos web y aplicaciones móviles y de escritorio",
+      empresa: "Axomy Solutions & Co.",
+      periodo: "2024 - 2025",
+    },
+    {
+      id: 4,
+      puesto: "Creador de sistemas de software para la prevencion de riesgos",
+      empresa: "Área Metropolitana del Valle de Aburrá",
+      periodo: "2024 - 2025",
+    },
+    {
+      id: 5,
+      puesto: "Desarrollador de Software",
+      empresa:
+        "Ministerio de Tecnologías de la Información y las Comunicaciones (MinTIC)",
+      periodo: "2023 - 2024",
+    },
+    {
+      id: 6,
+      puesto: "Desarrollador Backend",
+      empresa: "Heinsohn Business Technology",
+      periodo: "2023 - 2024",
+    },
+    {
+      id: 7,
+      puesto: "Colaborador de proyectos de software y produccion de contenido",
+      empresa: "Alcaldía de Girardota",
+      periodo: "2023 - 2024",
+    },
+    {
+      id: 8,
+      puesto: "Desarrollador de Software",
+      empresa: "Imagine Apps",
+      periodo: "Julio 2022 - Enero 2023",
+    },
+    {
+      id: 9,
+      puesto: "Desarrollador Full Stack",
+      empresa: "Pragma S.A.",
+      periodo: "Enero 2022 - Agosto 2022",
+    },
+    {
+      id: 10,
+      puesto: "Backend Developer",
+      empresa: "TEAM International",
+      periodo: "Enero 2022 - Junio 2022",
+    },
+  ];
+
+  if (experiencia.length === 0) {
+    return (
+      <section>
+        <h3>Experiencia laboral</h3>
+        <p>No se ha registrado experiencia laboral.</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h3>Experiencia Profesional</h3>
       <ul>
-        <li>
-          <strong>{experiencia.apps}</strong> - <p class="fecha">{empresas.axomy} (2022 - 2024)</p>
-        </li>
-        <li>
-          <strong>{experiencia.tecnologias}</strong>
-           - <p class="fecha">{empresas.amva} (2024 - 2025)</p>
-        </li>
-        <li>
-          <strong>{experiencia.asociaciones}</strong>
-           - <p class="fecha">{empresas.alcgir} (2025)</p>
-        </li>
+        {experiencia.map((exp) => (
+          <li key={exp.id}>
+            <strong>{exp.puesto}</strong> - {exp.empresa} ({exp.periodo})
+          </li>
+        ))}
       </ul>
     </section>
   );
