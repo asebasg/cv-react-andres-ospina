13be6d25fc9a1ab0a554f5faa907f212b75d04a0
Sebastián Ospina <asebasg07@gmail.com>
2025-06-04 14:39:57 -0500

    feat: Educacion.jsx ahora recibe props desde cvData.js

--- src/components/Educacion.jsx
+++ src/components/Educacion.jsx
@@ -1,42 +1,8 @@
 import "./Educacion.css";
 
-export default function Educacion() {
-  const educacion = [
-    {
-      id: 1,
-      titulo:
-        "Especializacion en Desarrollo y Gestión de Inteligencia Artificial",
-      institucion: "Universidad de los Andes",
-      periodo: "2030 - 2034",
-    },
-    {
-      id: 2,
-      titulo: "Ingenieria de Datos y Software",
-      institucion: "Universidad Coperativa de Colombia",
-      periodo: "2027 - 2029",
-    },
-    {
-      id: 3,
-      titulo: "Tecnologia en Analisis y Desarrollo de Software",
-      institucion: "SENA",
-      periodo: "2025 - 2026",
-    },
-    {
-      id: 4,
-      titulo: "Diplomado de Programación en PHP",
-      institucion: "Politécnico Grancolombiano",
-      periodo: "2024",
-    },
-    {
-      id: 5,
-      titulo:
-        "Bachiller técnico en Sistemas con énfasis en Desarrollo de Software",
-      institucion: "I.E. Atanasio Girardot",
-      periodo: "2019 - 2024",
-    },
-  ];
-
-  if (educacion.length === 0) {
+export default function Educacion({estudios}) {
+  // Si no hay estudios, mostrar un mensaje
+  if (!estudios || estudios.length === 0) {
     return (
       <section>
         <h3>Educación</h3>
@@ -48,9 +14,9 @@ export default function Educacion() {
     <section>
       <h3>Educación</h3>
       <ul>
-        {educacion.map((edu) => (
-          <li key={edu.id}>
-            <strong>{edu.titulo}</strong>- {edu.institucion} ({edu.periodo})
+        {estudios.map(({id, titulo, institucion, periodo}) => (
+          <li key={id}>
+            <strong>{titulo}</strong> - {institucion} ({periodo})
           </li>
         ))}
       </ul>
