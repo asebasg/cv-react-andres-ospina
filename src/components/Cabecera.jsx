d01bcdcd923c27de0ee3fbf542b08fa6fb915cdf
Sebastián Ospina <asebasg07@gmail.com>
2025-06-04 14:31:31 -0500

    feat: Cabecera.jsx, todo el componente recibe props desde cvData.js.

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -1,31 +1,11 @@
 import "./Cabecera.css" 
 
-const user = {
-  nombre: "Andrés Sebastián Ospina Guzmán",
-  cargo: "Desarrollador FullStack Senior",
-  telefono: "3225316150",
-  correo: "asebasg07@gmail.com",
-  ciudad: "Medellín",
-};
-
-export default function CabeceraCV() {
+export default function CabeceraCV({nombre, cargo}) {
   return (
     <header>
-      <h1>{user.nombre}</h1>
-      <h2>{user.cargo}</h2>
-      <h4>Datos de contacto:</h4>
-      <div class="contacto">
-      <p>
-        <strong>Teléfono</strong>: {user.telefono}
-      </p>
-      <p>
-        <strong>Correo</strong>: {user.correo}
-      </p>
-      <p>
-        <strong>Ciudad</strong>: {user.ciudad}
-      </p>
-      </div>
-      
+      <h1>{nombre}</h1>
+      <h2>{cargo}</h2>
+      <hr />
     </header>
-  );
+  )
 }
