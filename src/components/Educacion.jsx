9ebb68cfc89a1a71bf96b730a5de0a996b2bfd68
Sebastián Ospina <asebasg07@gmail.com>
2025-06-04 16:21:37 -0500

    feat: Educacion.jsx, todo el componente tiene props y esta desestructurado.

--- src/components/Educacion.jsx
+++ src/components/Educacion.jsx
@@ -1,25 +1,27 @@
-import "./Educacion.css";
+import React from "react";
 
-export default function Educacion({estudios}) {
-  // Si no hay estudios, mostrar un mensaje
+export default function Educacion({ estudios }) {
   if (!estudios || estudios.length === 0) {
     return (
       <section>
         <h3>Educación</h3>
-        <p>No se ha registrado educacion previa.</p>
+        <p>No se ha registrado formación académica.</p>
+        <hr />
       </section>
     );
   }
+
   return (
     <section>
       <h3>Educación</h3>
       <ul>
-        {estudios.map(({id, titulo, institucion, periodo}) => (
+        {estudios.map(({ id, titulo, universidad, periodo }) => (
           <li key={id}>
-            <strong>{titulo}</strong> - {institucion} ({periodo})
+            <strong>{titulo}</strong> - {universidad} ({periodo})
           </li>
         ))}
       </ul>
+      <hr />
     </section>
   );
 }
