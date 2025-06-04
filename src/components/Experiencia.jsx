8929966c93d963ba4eec88ed68b955d6f27f34b8
Sebastián Ospina <asebasg07@gmail.com>
2025-06-04 14:59:02 -0500

    feat: Experiencia.jsx, con props desde cvData.js.

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,72 +1,8 @@
 import "./Experiencia.css";
 
-export default function Experiencia() {
-  const experiencia = [
-    {
-      id: 1,
-      puesto: "Desarrollador Full Stack",
-      empresa: "ToGrow Agencia",
-      periodo: "2026 - Actualidad",
-    },
-    {
-      id: 2,
-      puesto: "Lider de proyectos de desarrollo de software",
-      empresa: "Gobernacion de Antioquia",
-      periodo: "2024 - 2026",
-    },
-    {
-      id: 3,
-      puesto:
-        "Creador de aplicativos web y aplicaciones móviles y de escritorio",
-      empresa: "Axomy Solutions & Co.",
-      periodo: "2024 - 2025",
-    },
-    {
-      id: 4,
-      puesto: "Creador de sistemas de software para la prevencion de riesgos",
-      empresa: "Área Metropolitana del Valle de Aburrá",
-      periodo: "2024 - 2025",
-    },
-    {
-      id: 5,
-      puesto: "Desarrollador de Software",
-      empresa:
-        "Ministerio de Tecnologías de la Información y las Comunicaciones (MinTIC)",
-      periodo: "2023 - 2024",
-    },
-    {
-      id: 6,
-      puesto: "Desarrollador Backend",
-      empresa: "Heinsohn Business Technology",
-      periodo: "2023 - 2024",
-    },
-    {
-      id: 7,
-      puesto: "Colaborador de proyectos de software y produccion de contenido",
-      empresa: "Alcaldía de Girardota",
-      periodo: "2023 - 2024",
-    },
-    {
-      id: 8,
-      puesto: "Desarrollador de Software",
-      empresa: "Imagine Apps",
-      periodo: "Julio 2022 - Enero 2023",
-    },
-    {
-      id: 9,
-      puesto: "Desarrollador Full Stack",
-      empresa: "Pragma S.A.",
-      periodo: "Enero 2022 - Agosto 2022",
-    },
-    {
-      id: 10,
-      puesto: "Backend Developer",
-      empresa: "TEAM International",
-      periodo: "Enero 2022 - Junio 2022",
-    },
-  ];
-
-  if (experiencia.length === 0) {
+export default function Experiencia({ trabajos }) {
+  // si no hay experiencia previa, mostrar mensaje
+  if (!trabajos || trabajos.length === 0) {
     return (
       <section>
         <h3>Experiencia laboral</h3>
@@ -79,11 +15,12 @@ export default function Experiencia() {
     <section>
       <h3>Experiencia Profesional</h3>
       <ul>
-        {experiencia.map((exp) => (
-          <li key={exp.id}>
-            <strong>{exp.puesto}</strong> - {exp.empresa} ({exp.periodo})
+        {trabajos.map(({ id, cargo, empresa, periodo }) => (
+          <li key={id}>
+            <strong>{cargo}</strong>- {empresa} ({periodo})
           </li>
         ))}
+        ;
       </ul>
     </section>
   );
